0a72fd13a7e2ebfe4c480d0a6f9c6ebe3678a9c8
sharonxz <ssharonzhengg@gmail.com>
2026-04-20 11:26:03 -0400

    guppy1

---
+++ guppy/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import Navigation from '@/components/Navigation';
+export default function About() {
+  return (
+    <div className="min-h-screen bg-white">
+    <Navigation>
+        
+    </Navigation>
+      </div>
+  )
+}
